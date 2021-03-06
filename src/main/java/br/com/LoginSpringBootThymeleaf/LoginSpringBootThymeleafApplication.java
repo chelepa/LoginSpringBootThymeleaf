66ca54d119f9a6c5acaed5f40d323043ca5c034c
Claudimir Chelepa <claudimir.chelepa@compasso.com.br>
2021-03-06 10:46:37 -0300

    primeiro commit

---
+++ src/main/java/br/com/LoginSpringBootThymeleaf/LoginSpringBootThymeleafApplication.java
@@ -0,0 +1,13 @@
+package br.com.LoginSpringBootThymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSpringBootThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginSpringBootThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/LoginSpringBootThymeleaf/LoginSpringBootThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.LoginSpringBootThymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginSpringBootThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
