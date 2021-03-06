39403882e03646f280a22e19876bcbe352053b09
Claudimir Chelepa <claudimir.chelepa@compasso.com.br>
2021-03-06 19:14:01 -0300

    Update Entities

--- src/main/java/br/com/LoginSpringBootThymeleaf/entities/PermissaoEntity.java
+++ src/main/java/br/com/LoginSpringBootThymeleaf/entities/PermissaoEntity.java
@@ -1,5 +1,28 @@
 package br.com.LoginSpringBootThymeleaf.entities;
 
-public class PermissaoEntity {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "TB_PERMISSAO")
+public class PermissaoEntity implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name="ID_PERMISSAO")
+	private Integer id;
+	 
+	@Column(name="DS_PERMISSAO")
+	private String permissao;
+	
+	@Column(name="DS_DESCRICAO")
+	private String descricao;
 }
--- src/main/java/br/com/LoginSpringBootThymeleaf/entities/UsuarioEntity.java
+++ src/main/java/br/com/LoginSpringBootThymeleaf/entities/UsuarioEntity.java
@@ -1,5 +1,35 @@
 package br.com.LoginSpringBootThymeleaf.entities;
 
-public class UsuarioEntity {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "TB_USUARIO")
+public class UsuarioEntity implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name="ID_USUARIO")
+	private Integer id;
+	 
+	@Column(name="DS_NOME")
+	private String nome;
+
+	@Column(name = "DS_LOGIN")
+	private String login;
+
+	@Column(name = "DS_SENHA")
+	private String senha;
+
+	@Column(name = "FL_ATIVO")
+	private boolean ativo;
+	
 }
