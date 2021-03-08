6e9058e74b59cf51fe26dc7677ab7bf9b05894cd
Claudimir Chelepa <claudimir.chelepa@compasso.com.br>
2021-03-08 15:53:05 -0300

    create WebSecurityConfig

--- src/main/java/br/com/LoginSpringBootThymeleaf/services/WebSecurityConfig.java
+++ src/main/java/br/com/LoginSpringBootThymeleaf/services/WebSecurityConfig.java
@@ -3,24 +3,21 @@ package br.com.LoginSpringBootThymeleaf.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
 	@Autowired
 	private UsuarioService usuarioRepositoryImpl;
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
- 
- 
 		http.authorizeRequests()
 			.antMatchers("/usuario/novoCadastro").access("hasRole('ADMIN') or hasRole('ROLE_CADASTROUSUARIO')")
 			.antMatchers("/usuario/consultar").access("hasRole('ADMIN') or hasRole('CONSULTAUSUARIO')")
@@ -35,13 +32,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.logoutSuccessUrl("/")
 				.logoutUrl("/logout") 
 				.permitAll();
+		
 		http.exceptionHandling().accessDeniedPage("/acessoNegado");
-		http.authorizeRequests().antMatchers("/resources/**").permitAll().anyRequest().permitAll();
+		
 	}
-	
+ 
 	@Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {				
 		auth.userDetailsService(usuarioRepositoryImpl).passwordEncoder(new BCryptPasswordEncoder());
     }
-
+	
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
+    }
 }
