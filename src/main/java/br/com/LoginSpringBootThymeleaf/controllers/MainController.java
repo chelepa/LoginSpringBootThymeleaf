68115a029727738af47f51331084e44828cf1684
Claudimir Chelepa <claudimir.chelepa@compasso.com.br>
2021-03-12 08:41:08 -0300

    Update das folhas de estilos ainda falta ajustar

--- src/main/java/br/com/LoginSpringBootThymeleaf/controllers/MainController.java
+++ src/main/java/br/com/LoginSpringBootThymeleaf/controllers/MainController.java
@@ -10,5 +10,23 @@ public class MainController {
 	public String login(){
 	    return "/login/Login.html";
 	}
+
+	@GetMapping(value="/home")
+	public String home(){
+ 
+		return "/home/home.html";
+	}
+	
+	@GetMapping(value="/acessoNegado")
+	public String acessoNegado(){
+ 
+		return "/erros/AcessoNegado.html";
+	}
+	
+	@GetMapping(value="/layout")
+	public String layout(){
+ 
+		return "/layout/layout.html";
+	}
  
 }
